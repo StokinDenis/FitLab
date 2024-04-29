8e0571c9d69d0b8d6ddb0facd6ed3fc706f99428
Stokin Denis <stokindenis@gmail.com>
2024-04-29 10:52:54 +0300

    [FIT-4] add routing

--- FitLab/src/app/components/fitness-services-page/fitness-services-page.component.ts
+++ FitLab/src/app/components/fitness-services-page/fitness-services-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HeaderComponent} from "../header/header.component";
+import {FooterComponent} from "../footer/footer.component";
 
 @Component({
   selector: 'app-fitness-services-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './fitness-services-page.component.html',
   styleUrls: ['./fitness-services-page.component.scss']
 })
--- FitLab/src/app/components/home-page/home-page.component.ts
+++ FitLab/src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {FitMapComponent} from "../fit-map/fit-map.component";
+import {FooterComponent} from "../footer/footer.component";
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+    imports: [CommonModule, FitMapComponent, FooterComponent, HeaderComponent],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
