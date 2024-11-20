c75ed55c5b88bdeebfc799a1f7938b55d7674ca7
Stokin Denis <stokindenis@gmail.com>
2024-11-20 16:08:40 +0300

    [FIT-5]

--- FitLab/src/app/app.component.ts
+++ FitLab/src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import {HomePageComponent} from "./components/home-page/home-page.component";
-import {HeaderComponent} from "./components/header/header.component";
-import {FooterComponent} from "./components/footer/footer.component";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HomePageComponent, HeaderComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: "app-root",
+    standalone: true,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [CommonModule, RouterOutlet, HomePageComponent, HeaderComponent, FooterComponent],
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'FitLab';
+    title = "FitLab";
 }
